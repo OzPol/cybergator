72248fe0f1aa112d1f36e5c769ef4e86588ead76
jess <70455379+jesslourenco@users.noreply.github.com>
2025-02-21 19:42:55 -0300

    simple setup working (no dbs)

---
+++ app.py
@@ -0,0 +1,10 @@
+import dash
+from dash import html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div("Hello, World!")
+
+if __name__ == "__main__":
+    app.run_server(debug=True, host="0.0.0.0", port=8000)
+
