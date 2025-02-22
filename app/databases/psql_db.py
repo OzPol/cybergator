c031c138bed900483f5469cb1c6488106b349ed3
jess <70455379+jesslourenco@users.noreply.github.com>
2025-02-21 23:09:20 -0300

    setup working with supabase and neo4j aura :)

--- app/databases/psql_db.py
+++ app/databases/psql_db.py
@@ -10,8 +10,8 @@ def get_sql_connection():
     """Creates and returns a new postgres database connection."""
     try:
         conn = psycopg2.connect(POSTGRES_URL)
-        print("Connection successful!")
+        print("✅ Supabase Connection successful!")
         return conn
     except psycopg2.Error as e:
-        print("Database connection error:", e)
+        print("❌ Supabase connection error:", e)
         return None
