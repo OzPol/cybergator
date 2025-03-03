cdcfa0a76ed1d38603de7140530a8b7fc7037536
Shayan414 <shayanakh@gmail.com>
2025-03-03 18:53:13 -0500

    Fixed page layout after login issue

--- application/views/pages/dashboard.py
+++ application/views/pages/dashboard.py
@@ -4,10 +4,5 @@ import dash_bootstrap_components as dbc
 def dashboard(session_user):
     return html.Div(
         html.H1(f"Welcome {session_user}! You are logged in."),
-        id="page-content",
-        style={
-            "marginLeft": "15%",  # To account for the sidebar width
-            "marginTop": "80px",   # To account for the fixed banner height
-            "padding": "20px",     # Adds spacing around the content
-        },
+        id="page-content"
     )
