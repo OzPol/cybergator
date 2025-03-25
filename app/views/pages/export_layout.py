37d2a8de7f698451e333be951f6304a19119df43
Samson Carter <129422671+SamsonCart@users.noreply.github.com>
2025-03-25 18:38:28 -0400

    add body copy/adjust export page to match system tables

--- app/views/pages/export_layout.py
+++ app/views/pages/export_layout.py
@@ -1,6 +1,15 @@
 from dash import html
 import dash_bootstrap_components as dbc
 
+# Table list that matches table pages in app/views/pages
+system_tables_options = [
+    {"label": "Nodes", "value": "nodes"},
+    {"label": "CVEs", "value": "cves"},
+    {"label": "Software Nodes", "value": "software-nodes"},
+    {"label": "Critical Functions", "value": "critical-functions"},
+    {"label": "Unique Software", "value": "software-unique"},
+]
+
 def export_layout():
     return dbc.Container([
         html.H2("Export Data", className="text-center mb-4"),
@@ -9,11 +18,14 @@ def export_layout():
         dbc.Row([
             dbc.Col([
                 html.P("CyberGator allows users to generate detailed reports on their system’s resilience assessments.", className="mb-3 text-center"),
-                html.Ul([
-                    html.Li("Download reports in CSV format."),
-                    html.Li("Share findings with security teams for further analysis."),
-                    html.Li("Maintain historical records to track improvements over time."),
-                ], className="mb-3"),
+                html.Div([
+                    html.Ul([
+                        html.Li("Download reports in CSV or PDF format."),
+                        html.Li("Share findings with security teams for further analysis."),
+                        html.Li("Maintain historical records to track improvements over time."),
+                    ])
+                ], className="mb-3", style={"maxWidth": "800px", "margin": "0 auto"}),  # centers and limits width
+
                 html.P("These reports provide valuable documentation for cybersecurity planning and compliance audits.", className="mb-4 text-center"),
             ], width=12)
         ]),
@@ -23,11 +35,7 @@ def export_layout():
 
         # Checklist
         dbc.Checklist(
-            options=[
-                {"label": "Data Table A", "value": "A"},
-                {"label": "Data Table B", "value": "B"},
-                {"label": "Data Table C", "value": "C"}
-            ],
+            options=system_tables_options,
             id="data-tables-checklist",
             inline=True,
             className="mb-4"
