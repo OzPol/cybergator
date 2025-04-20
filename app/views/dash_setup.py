e7cb7bd23e20eb7d9489a3fb406220f6e8f59a26
Ozlem <95313700+OzPol@users.noreply.github.com>
2025-04-19 20:53:46 -0400

    Add  dual PyVis attack tree visualizations: logical tree and system node mapping
    1. Logical attack tree: Goal → Step → Critical Function
    
    2. Critical function mapping: CF → System Nodes

--- app/views/dash_setup.py
+++ app/views/dash_setup.py
@@ -23,6 +23,8 @@ from app.views.ui_text.jess_bio import about_me_bio
 from app.views.ui_text.oz_bio import oz_bio
 from app.views.pages.apt_simulation import simulation_apt_layout
 from app.views.pages.cve_simulation import cve_simulation_layout
+from app.views.pages.attack_tree import attack_tree_layout
+
 
 def render_page_content(pathname, session_user):
     """Dynamically updates the content area based on the session"""
@@ -76,6 +78,9 @@ def render_page_content(pathname, session_user):
         return simulation_apt_layout()
     elif pathname == "/cve-simulation":
         return cve_simulation_layout()
+    elif pathname == "/fsm-simulation":
+        return attack_tree_layout()
+    
     return html.H1("404 - Page Not Found", className="text-center")
 
 def get_main_layout():
